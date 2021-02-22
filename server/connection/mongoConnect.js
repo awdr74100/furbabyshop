894131dfd8dff851d44737b16b1f02335048f0b7
藍奕濡 <a78945612385238@gmail.com>
2021-02-23 01:52:51 +0800

    add connected comment

--- server/connection/mongoConnect.js
+++ server/connection/mongoConnect.js
@@ -1,13 +1,15 @@
 import mongoose from 'mongoose';
 
-export default () => {
-  return mongoose
-    .connect(process.env.DATABASE_URL, {
+export default async () => {
+  try {
+    await mongoose.connect(process.env.DATABASE_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false,
       useCreateIndex: true,
-    })
-    .then(() => console.log('DB Connected！'))
-    .catch((err) => console.log(err));
+    });
+    console.log(`Connected DB: ${process.env.DATABASE_URL}`);
+  } catch (error) {
+    console.log(error.message);
+  }
 };
